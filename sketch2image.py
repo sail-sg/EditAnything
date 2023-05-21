4110c69ebdf5c97c9095a80c31745abfe38f9059
Zhijie Lin <136380243@qq.com>
2023-05-21 21:42:54 +0800

    update

--- sketch2image.py
+++ sketch2image.py
@@ -262,4 +262,4 @@ def create_demo():
 
 if __name__ == '__main__':
     demo = create_demo()
-    demo.queue().launch(server_name='0.0.0.0')
+    demo.queue().launch(server_name='0.0.0.0', share=True)
