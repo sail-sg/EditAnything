c08bc9cfef3ada29911c45b773dbf227dd3e575c
shgao <shgao@live.com>
2023-06-25 21:42:06 +0800

    change sd21 to sd15

--- editany.py
+++ editany.py
@@ -28,11 +28,9 @@ def create_demo(process, process_image_click=None):
 
 if __name__ == "__main__":
     model = EditAnythingLoraModel(
-        base_model_path="stabilityai/stable-diffusion-2",
-        controlmodel_name="LAION Pretrained(v0-4)-SD21",
-        extra_inpaint=False,
-        lora_model_path=None,
-        use_blip=True,
+        base_model_path="runwayml/stable-diffusion-v1-5",
+        controlmodel_name='LAION Pretrained(v0-4)-SD15',
+        lora_model_path=None, use_blip=True, extra_inpaint=True,
     )
     demo = create_demo(model.process, model.process_image_click)
     demo.queue().launch(server_name="0.0.0.0")
